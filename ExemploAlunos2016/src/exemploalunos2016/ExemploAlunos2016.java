4c30dd90e227c89a3f034975c048679fd65614ee
comp1 <comp1@comp1_01.lab.unesc.net>
2016-04-01 20:10:55 -0300

    Inicializado o JFrame no main

--- ExemploAlunos2016/src/exemploalunos2016/ExemploAlunos2016.java
+++ ExemploAlunos2016/src/exemploalunos2016/ExemploAlunos2016.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exemploalunos2016;
 
-/**
- *
- * @author comp1
- */
 public class ExemploAlunos2016 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new PessoaJFrame().setVisible(true);
     }
     
 }
